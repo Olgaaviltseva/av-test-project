db2cfe905b6c11ac071a2ba9cf015b3b72ec491d
OlgaAviltseva <Olgaaviltseva@gmail.com>
2023-02-11 21:59:12 +0100

    9 test

--- pages/search_form_page.py
+++ pages/search_form_page.py
@@ -1,9 +1,141 @@
 from pages.base_page import BasePage
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from pages.locators import search_form_page as loc
+from selenium.webdriver.common.by import By
 
 
 class SearchPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-
     def open(self):
+        self.driver.get('https://av.by/')
+
+    def scroll_page_450(self):
+        self.driver.execute_script("window.scrollTo(0, 450);")
+
+    def click_first_brand_drop_down(self):
+        self.find(loc.first_brand_drop_down).click()
+
+    def click_first_model_drop_down(self):
+        self.find(loc.first_model_drop_down).click()
+
+    def click_first_generation_drop_down(self):
+        self.find(loc.first_generation_drop_down).click()
+
+    def click_bentley_brand(self):
+        self.find(loc.bentley_button).click()
+
+    def click_audi_brand(self):
+        self.find(loc.audi_button).click()
+
+    def click_a3_model(self):
+        self.find(loc.a3_button).click()
+
+    def click_gen_8l_button(self):
+        self.find(loc.gen_8l_button).click()
+
+    def click_exclude_checkbox(self):
+        self.find(loc.exclude_checkbox).click()
+
+    def click_show_button(self):
+        self.find(loc.show_button).click()
+
+    def click_year_from_button(self):
+        self.find(loc.year_from_button).click()
+
+    def click_year_before_button(self):
+        self.find(loc.year_before_button).click()
+
+    def click_year_from_2022_button(self):
+        self.find(loc.year_from_2022_button).click()
+
+    def click_year_before_2022_button(self):
+        self.find(loc.year_before_2022_button).click()
+
+    def click_price_from_button(self):
+        self.find(loc.price_from_button).click()
+
+    def click_price_before_button(self):
+        self.find(loc.price_before_button).click()
+
+    def click_currency_button(self):
+        self.find(loc.currency_button).click()
+
+    def click_byn_button(self):
+        self.find(loc.byn_button).click()
+
+    def get_text_bentley_brand(self):
+        return self.find(loc.bentley_brand).get_attribute('innerText')
+
+    def get_text_excl_bentley_brand(self):
+        return self.find(loc.bentley_brand_excl).get_attribute('innerText')
+
+    def get_text_a3_model(self):
+        return self.find(loc.a3_model).get_attribute('innerText')
+
+    def get_text_2022_year_from(self):
+        return self.find(loc.year_2022_from).get_attribute('innerText')
+
+    def get_text_2022_year_before(self):
+        return self.find(loc.year_2022_before).get_attribute('innerText')
+
+    def get_text_price_from_500(self):
+        return self.find(loc.price_from_500).get_attribute('innerText')
+
+    def get_text_price_before_5000(self):
+        return self.find(loc.price_before_5000).get_attribute('innerText')
+
+    def get_text_byn_currency(self):
+        return self.find(loc.byn_currency).get_attribute('innerText')
+
+    def wait_listing_title(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.listing__title))
+
+    def wait_show_button_bentley(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_bentley))
+
+    def wait_show_button_audi_a3(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_audi_a3))
+
+    def wait_show_button_excl_bentley(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_excl_bentley))
+
+    def wait_show_button_8l(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_audi_a3_8l))
+
+    def wait_show_button_from_2022(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_from_2022))
+
+    def wait_show_button_before_2022(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_before_2022))
+
+    def wait_a3_active(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.a3_list))
+
+    def wait_8l_active(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.list_8l))
+
+    def wait_byn_currency(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_byn))
+
+    def wait_show_button_from_price_500(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_from_price_500))
+
+    def wait_show_button_before_price_5000(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_before_price_5000))
+
+    def wait_show_button_5000_byn(self):
+        WebDriverWait(self.driver, 10).until(ec.presence_of_element_located(loc.show_button_5000_byn))
+
+    def get_text_list_result(self):
+        list_result = self.driver.find_element(By.CLASS_NAME, 'listing__items')
+        list_cars = list_result.find_elements(By.CLASS_NAME, 'link-text')
+        return list_cars[5].get_attribute('innerText')
+
+    def send_price_from_500(self):
+        self.send(loc.price_from_button, '500')
+
+    def send_price_before_5000(self):
+        self.send(loc.price_before_button, '5000')
--- tests/test_search_param.py
+++ tests/test_search_param.py
@@ -1,63 +1,177 @@
 from pages.search_form_page import SearchPage
-from selenium.webdriver.common.by import By
+import allure
 from time import sleep
 
 
+@allure.feature('Search form page')
+@allure.story('Search by brand')
 def test_search_choose_brand(driver):
-    search_page = SearchPage(driver)
-    search_page.open()
-    search_page.scroll_page_450()
-    search_page.click_first_brand_drop_down()
-    search_page.click_bentley_brand()
-    search_page.wait_show_button_bentley()
-    search_page.click_show_button()
-    assert search_page.get_text_bentley_brand() == 'Bentley'
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select brand'):
+        search_page.click_first_brand_drop_down()
+        search_page.click_bentley_brand()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_bentley()
+        search_page.click_show_button()
+    with allure.step('Check that brand is selected'):
+        assert search_page.get_text_bentley_brand() == 'Bentley'
 
 
+@allure.feature('Search form page')
+@allure.story('Search with an excluded brand')
 def test_search_exclude_brand(driver):
-    search_page = SearchPage(driver)
-    search_page.open()
-    search_page.scroll_page_450()
-    search_page.click_first_brand_drop_down()
-    search_page.click_exclude_checkbox()
-    search_page.click_bentley_brand()
-    search_page.wait_show_button_excl_bentley()
-    search_page.click_show_button()
-    assert search_page.get_text_excl_bentley_brand() == 'Кроме'
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Exclude Bentley brand'):
+        search_page.click_first_brand_drop_down()
+        search_page.click_exclude_checkbox()
+        search_page.click_bentley_brand()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_excl_bentley()
+        search_page.click_show_button()
+    with allure.step('Check that brand is excluded'):
+        assert search_page.get_text_excl_bentley_brand() == 'Кроме'
 
 
+@allure.feature('Search form page')
+@allure.story('Search by brand and model')
 def test_search_model(driver):
-    search_page = SearchPage(driver)
-    search_page.open()
-    search_page.scroll_page_450()
-    search_page.click_first_brand_drop_down()
-    search_page.click_audi_brand()
-    search_page.wait_a3_active()
-    search_page.click_first_model_drop_down()
-    search_page.click_a3_model()
-    search_page.wait_show_button_audi_a3()
-    search_page.click_show_button()
-    assert search_page.get_text_a3_model() == 'A3'
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select Audi brand'):
+        search_page.click_first_brand_drop_down()
+        search_page.click_audi_brand()
+    with allure.step('Select A3 model'):
+        search_page.wait_a3_active()
+        search_page.click_first_model_drop_down()
+        search_page.click_a3_model()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_audi_a3()
+        search_page.click_show_button()
+    with allure.step('Check that model is selected'):
+        assert search_page.get_text_a3_model() == 'A3'
 
 
+@allure.feature('Search form page')
+@allure.story('Search by brand, model and generation')
 def test_search_generation(driver):
-    search_page = SearchPage(driver)
-    search_page.open()
-    search_page.scroll_page_450()
-    search_page.click_first_brand_drop_down()
-    search_page.click_audi_brand()
-    search_page.wait_a3_active()
-    search_page.click_first_model_drop_down()
-    search_page.click_a3_model()
-    search_page.wait_8l_active()
-    search_page.click_first_generation_drop_down()
-    search_page.click_gen_8l_button()
-    search_page.wait_show_button_8l()
-    search_page.click_show_button()
-    assert search_page.get_text_list_result() == 'Audi A3 8L'
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select Audi brand'):
+        search_page.click_first_brand_drop_down()
+        search_page.click_audi_brand()
+    with allure.step('Select A3 model'):
+        search_page.wait_a3_active()
+        search_page.click_first_model_drop_down()
+        search_page.click_a3_model()
+    with allure.step('Select 8l generation'):
+        search_page.wait_8l_active()
+        search_page.click_first_generation_drop_down()
+        search_page.click_gen_8l_button()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_8l()
+        search_page.click_show_button()
+    with allure.step('Check that generation is selected'):
+        assert search_page.get_text_list_result() == 'Audi A3 8L'
 
 
+@allure.feature('Search form page')
+@allure.story('Search from the year')
 def test_search_year_from(driver):
-    search_page = SearchPage(driver)
-    search_page.open()
-    search_page.scroll_page_450()
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select from 2022 year'):
+        search_page.click_year_from_button()
+        search_page.click_year_from_2022_button()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_from_2022()
+        search_page.click_show_button()
+    with allure.step('Check that from 2022 year is selected'):
+        assert search_page.get_text_2022_year_from() == '2022'
+
+
+@allure.feature('Search form page')
+@allure.story('Search by min year')
+def test_search_year_before(driver):
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select before 2022 year'):
+        search_page.click_year_before_button()
+        search_page.click_year_before_2022_button()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_before_2022()
+        search_page.click_show_button()
+    with allure.step('Check that before 2022 year is selected'):
+        assert search_page.get_text_2022_year_before() == '2022'
+
+
+@allure.feature('Search form page')
+@allure.story('Search by min price')
+def test_search_price_from(driver):
+    with allure.step('Open Search from page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select price from'):
+        search_page.click_price_from_button()
+        search_page.send_price_from_500()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_from_price_500()
+        search_page.click_show_button()
+    with allure.step('Check that from price is selected'):
+        assert search_page.get_text_price_from_500() == 'Цена от'
+
+
+@allure.feature('Search form page')
+@allure.story('Search by max price')
+def test_search_price_before(driver):
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select price before'):
+        search_page.click_price_before_button()
+        search_page.send_price_before_5000()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_before_price_5000()
+        search_page.click_show_button()
+    with allure.step('Check that before price is selected'):
+        assert search_page.get_text_price_before_5000() == 'до'
+
+
+@allure.feature('Search form page')
+@allure.story('Currency selection')
+def test_search_currency_byn(driver):
+    with allure.step('Open Search form page'):
+        search_page = SearchPage(driver)
+        search_page.open()
+        search_page.scroll_page_450()
+    with allure.step('Select BYN'):
+        search_page.click_currency_button()
+        search_page.click_byn_button()
+    with allure.step('Select price before'):
+        search_page.wait_byn_currency()
+        search_page.click_price_before_button()
+        search_page.send_price_before_5000()
+    with allure.step('Click show button'):
+        search_page.wait_show_button_5000_byn()
+        search_page.click_show_button()
+    with allure.step('Check that BYN currency is selected'):
+        assert search_page.get_text_byn_currency() == 'BYN'
+
+
+
+
